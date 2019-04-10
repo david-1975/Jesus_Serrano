9015fa2626c61fe21b3a39184a28e1dc4061413c
chuchi <chuchi@clase.es>
2019-04-10 13:58:53 +0200

    2bis

---
+++ IFCD0110/MF0951_2/UF1306/01_practicas_a_entregar/02 Imagen LightBox BIS/js/funciones.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+function imagen(a){	
+	var foto = 	"img" + a;
+	var tit = "titulo" + a;
+	alert(document.getElementById("tit").value)  ;
+	/*document.getElementById(foto).innerHTML = foto;*/
+	/*document.getElementById("todo").style.display = "block";
+	document.getElementById("imagen1").style.display = "block";*/	
+	
+}
+
+function cerrar(){
+	document.getElementById("todo").style.display="none";
+	document.getElementById("imagen1").style.display="none";
+}
--- IFCD0110/MF0951_2/UF1306/01_practicas_a_entregar/02 Imagen LightBox/js/funciones.js
+++ IFCD0110/MF0951_2/UF1306/01_practicas_a_entregar/02 Imagen LightBox/js/funciones.js
@@ -1,44 +1,44 @@
 // JavaScript Document
-function foto1(){
+function foto1(){		
+	document.getElementById("imagen1").style.display = "block";
 	document.getElementById("todo").style.display = "block";
-	document.getElementById("imagen1").style.width = "80%";
-	
 }
 function foto2(){
 	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen2").style.display = "block";	
 }
 function foto3(){
 	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen3").style.display = "block";	
 }
 function foto4(){
 	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen4").style.display = "block";	
 }
 function foto5(){
-	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen5").style.display = "block";
+	document.getElementById("todo").style.display = "block";	
 }
 function foto6(){
-	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen6").style.display = "block";
+	document.getElementById("todo").style.display = "block";	
 }
 function foto7(){
-	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen7").style.display = "block";
+	document.getElementById("todo").style.display = "block";	
 }
 function foto8(){
-	document.getElementById("todo").style.display = "block";
-	
-	
+	document.getElementById("imagen8").style.display = "block";
+	document.getElementById("todo").style.display = "block";	
 }
 function cerrar(){
-	document.getElementById("todo").style.display = "none";
+	document.getElementById("todo").style.display="none";
+	document.getElementById("imagen1").style.display="none";
+	document.getElementById("imagen2").style.display="none";
+	document.getElementById("imagen3").style.display="none";
+	document.getElementById("imagen4").style.display="none";
+	document.getElementById("imagen5").style.display="none";
+	document.getElementById("imagen6").style.display="none";
+	document.getElementById("imagen7").style.display="none";
+	document.getElementById("imagen8").style.display="none";
 }
